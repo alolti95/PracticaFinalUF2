3075f4194d0276cbfeb233ef36f8e9f22eb33644
Alejandro <Alejandro>
2017-03-07 12:09:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/user1/practicafinaluf2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user1.practicafinaluf2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAnimacion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAnimacion = (Button)findViewById(R.id.btnAnimacion);
+
+        btnAnimacion.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),animaciones.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/user1/practicafinaluf2/animaciones.java
@@ -0,0 +1,13 @@
+package com.example.user1.practicafinaluf2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class animaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animaciones);
+    }
+}
